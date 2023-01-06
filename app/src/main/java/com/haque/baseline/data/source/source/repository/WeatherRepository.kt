c270fba9eee87591353f59118049efed1071f47f
Liana Haque <haque.liana@gmail.com>
2023-01-06 12:55:46 -0800

    rewrote the repository.

--- app/src/main/java/com/haque/baseline/data/source/source/repository/WeatherRepository.kt
+++ app/src/main/java/com/haque/baseline/data/source/source/repository/WeatherRepository.kt
@@ -7,15 +7,11 @@ import com.haque.baseline.domain.Resource
 
 
 interface WeatherRepository {
-
-    // need a class that compiles all the weather data together.  Get all the data from one call.
-    suspend fun getCurrentWeatherData(lat: Double, long: Double): Resource<CurrentWeatherData>
-    suspend fun getHourlyWeatherData(lat:Double, long: Double): Resource<HourlyWeatherData>
-    suspend fun getDailyForecastedData(lat: Double, long: Double): Resource<HourlyWeatherData>
+    suspend fun getOneCallWeatherPayload(lat: Double, long: Double): Resource<OneCallWeatherPayload>
 }
 
 /*
 Repositories can be used to cache data onto the machine in the form of a RoomDataBase, or, as a
 Remote Data Source. Again. Repositories take form of RoomDatabases to save data onto the
  device, or, as  Room Data Sources.
- */
+*/
